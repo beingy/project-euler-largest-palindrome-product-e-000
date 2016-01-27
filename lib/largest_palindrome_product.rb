9732b6e9074470f1d62a3897946157b5ea57a12d
beingy <beingy@gmail.com>
2016-01-27 01:45:35 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+  first_number = 100
+  largest_palindrome = 1
+  while first_number <= 999
+    (first_number..999).each do |second_number|
+      product = first_number * second_number
+      if product.to_s == product.to_s.reverse
+        #puts "Palindrome found: #{product}"
+        #puts "#{first_number},#{second_number}"
+        if product > largest_palindrome
+          largest_palindrome = product
+          #puts "#{first_number},#{second_number}"
+        end
+      end
+    end
+    first_number += 1
+  end
+  #puts largest_palindrome
+  largest_palindrome
+end
+
+#largest_palindrome_product
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  
+def answer
+  first_number = 100
+  largest_palindrome = 1
+  while first_number <= 999
+    (first_number..999).each do |second_number|
+      product = first_number * second_number
+      if product.to_s == product.to_s.reverse
+        if product > largest_palindrome
+          largest_palindrome = product
+        end
+      end
+    end
+    first_number += 1
+  end  
+  largest_palindrome
+end
+  
+end
